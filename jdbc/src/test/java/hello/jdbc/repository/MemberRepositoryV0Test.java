a8a07826792b64cd620105f0443ebe98a4e66b20
권길남 <nimpa3201@naver.com>
2024-10-27 15:45:07 +0900

    Test: MemberRepositoryV0 클래스의 save() 메서드 테스트를 위한 crud() 테스트 메서드 추가

---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,20 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+@Slf4j
+class MemberRepositoryV0Test {
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        log.info("Executing save method");
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+
+    }
+}

eac12997a46d05dd9259c81d7c15be9282d7af5d
권길남 <nimpa3201@naver.com>
2024-10-28 21:01:29 +0900

    Test: findById 메서드의 조회 결과를 검증하는 코드 추가

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,9 +2,12 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -13,8 +16,16 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         log.info("Executing save method");
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
+
+
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}",findMember);
+        assertThat(findMember).isEqualTo(member);
 
     }
 }

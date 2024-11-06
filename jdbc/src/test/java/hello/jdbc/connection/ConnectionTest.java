2ae20e907567266c8bab362dfd7b996e8bd4d189
권길남 <nimpa3201@naver.com>
2024-11-06 21:56:23 +0900

    Test: DataSource 사용하여 커넥션 획득

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -3,7 +3,9 @@ package hello.jdbc.connection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -20,7 +22,35 @@ public class ConnectionTest {
         log.info("connection1={}, class={}",connection1,connection1.getClass());
         log.info("connection2={}, class={}",connection2,connection2.getClass());
 
+        //DriverManager 커넥션을 획득할 때마다 URl,USERNAME,PASSWORD 같은 파라미터 전달
+
     }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverMangerSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+
+        //DataSource를 사용하는 방식은 처음 객체를 생성할때만 필요한 파라미터 남겨두고 커넥션을 획득할때는
+        // dataSource.getConection()만 호출하면 된다.
+        // 설정과 사용 분리 !
+
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection connection1 = dataSource.getConnection();
+        Connection connection2 = dataSource.getConnection();
+
+        log.info("connection1={}, class={}",connection1,connection1.getClass());
+        log.info("connection2={}, class={}",connection2,connection2.getClass());
+
+    }
+
+
+
+
+
 
 
 

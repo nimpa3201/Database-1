9f06478e43631eced4e9d4c6317b3459de3e6031
권길남 <nimpa3201@naver.com>
2024-11-06 22:49:49 +0900

    Test: DataSource를 통해 커넥션 풀을 사용하는 예제

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,6 +1,7 @@
 package hello.jdbc.connection;
 
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -38,9 +39,37 @@ public class ConnectionTest {
 
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource); //활성화 2개 대기 8개
+        Thread.sleep(1000);
+
+        //커넥션 개수가 Pool의 maxpool 보다 많은경우 time out 발생 useDataSource 주석풀면 확인 가능
+
+
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection connection1 = dataSource.getConnection();
         Connection connection2 = dataSource.getConnection();
+//        Connection connection3 = dataSource.getConnection();
+//        Connection connection4 = dataSource.getConnection();
+//        Connection connection5 = dataSource.getConnection();
+//        Connection connection6 = dataSource.getConnection();
+//        Connection connection7 = dataSource.getConnection();
+//        Connection connection8 = dataSource.getConnection();
+//        Connection connection9 = dataSource.getConnection();
+//        Connection connection10 = dataSource.getConnection();
+//        Connection connection11  = dataSource.getConnection();
+
 
         log.info("connection1={}, class={}",connection1,connection1.getClass());
         log.info("connection2={}, class={}",connection2,connection2.getClass());

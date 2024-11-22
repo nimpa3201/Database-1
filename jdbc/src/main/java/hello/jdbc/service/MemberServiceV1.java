433489332c0f5b02e9d5967611d5ce575dbbebd9
권길남 <nimpa3201@naver.com>
2024-11-22 23:28:47 +0900

    Add: 주석 추가

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -13,6 +13,8 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        //트랜잭션 시작!
+
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
@@ -21,6 +23,8 @@ public class MemberServiceV1 {
         validation(toMember); // 검증에서 문제 생기면 예외 발생
 
         memberRepository.update(toId, toMember.getMoney()+money);
+
+        // 문제 없으면 커밋, 있으면 롤백 (트랜잭션 종료)
     }
 
     private void validation(Member toMember) {
